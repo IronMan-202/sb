7cba86a781f43e868bda7665296a55325771dfe5
pengda222 <12345678>
2018-05-10 18:49:33 +0800

    配置Zuul路由跳转

--- management/src/main/java/com/pengda/config/WebMvcConfigurer.java
+++ management/src/main/java/com/pengda/config/WebMvcConfigurer.java
@@ -43,11 +43,16 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);// 允许cookies跨域
-        corsConfiguration.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
-        corsConfiguration.addAllowedHeader("*");// #允许访问的头信息,*表示全部
-        corsConfiguration.setMaxAge(18000L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
-        corsConfiguration.addAllowedMethod("*");//允许所有的请求方式
+        // 允许cookies跨域
+        corsConfiguration.setAllowCredentials(true);
+        // #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        corsConfiguration.addAllowedOrigin("*");
+        // #允许访问的头信息,*表示全部
+        corsConfiguration.addAllowedHeader("*");
+        // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        corsConfiguration.setMaxAge(18000L);
+        //允许所有的请求方式
+        corsConfiguration.addAllowedMethod("*");
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
